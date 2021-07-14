d5551d6d9a5f202436f47f6725c55c9e3f9339fa
semimaks <semimaks@mail.ru>
2021-07-14 23:31:17 +0300

    Добавляем в репозиторий.

--- line_graph.py
+++ line_graph.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 
 def main():
     # Создать списки для координат X и Y каждой точки данных.
-    x_coords = [0, 1, 2, 3, 4]
-    y_coords = [0, 3, 1, 5, 2]
+    x_coords = [0, 1, 2, 3, 4, 5]
+    y_coords = [0, 3, 1, 5, 2, 4]
 
     # Построить линейный график.
     plt.plot(x_coords, y_coords)
