6df298a855816098ca6d98df2d93c3703927a5dd
Maksim <semimaks@mail.ru>
2021-07-15 11:18:01 +0300

    Кортежи

---
+++ tuple.py
@@ -0,0 +1,10 @@
+tup_1 = (1, 2, 3, 4, 5)
+print(tup_1)
+for num in tup_1:
+    print(num)
+print('-' * 25)
+for i in range(len(tup_1)):
+    print(tup_1[i])
+
+my_tuple = (1,)
+print(type(my_tuple))
