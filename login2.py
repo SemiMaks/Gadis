ef2e5600bf2043152d270deb6f766a69d77d66e0
Maksim <semimaks@mail.ru>
2021-07-21 11:57:41 +0300

    Добавлен код валидации пароля

--- login2.py
+++ login2.py
@@ -23,3 +23,42 @@ def get_login_name(first, last, idnumber):
 
     # Вернуть имя для входа в систему
     return login_name
+
+
+# Функция valid_password принимает пароль в качестве
+# аргумента и возвращает истину/ложь, сообщая о его
+# допустимости. Допустим пароль: не менее 7 символов,
+# минимум один символ в верхнем регистре, один в нижнем
+# и одну цифру.
+
+
+def valid_passsword(password):
+    # Назначить булевым переменным False.
+    correct_length = False
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+
+    # Приступаем к валидации.
+    if len(password) >= 7:
+        correct_length = True
+        # Проанализировать каждый символ и выставить
+        # соответствующий флаг, когда символ найден.
+        for ch in password:
+            if ch.isupper():
+                has_uppercase = True
+            if ch.islower():
+                has_lowercase = True
+            if ch.isdigit():
+                has_digit = True
+        # Определить все ли требования к паролю выполнены.
+        # Если Да, то назначить is_valed значение True.
+        # В противном случае назначить is_valid значение False.
+        if correct_length and has_uppercase and \
+                has_lowercase and has_digit:
+            is_valid = True
+        else:
+            is_valid = False
+
+        # Вернуть переменную is_valid
+        return is_valid
