7c72a5df6f7ad95c87cbf7da6fa1f8b2ae0accb6
Maksim <semimaks@mail.ru>
2021-07-22 18:18:22 +0300

    Добавлены строковые методы

--- dict_in_not_in.py
+++ dict_in_not_in.py
@@ -13,7 +13,7 @@ def main():
 
     # Добавляем/заменяем (если есть такой ключ- значение обновится) в словарь Джуди
     phone_book['Джуди'] = '555-555-333'
-    if'Джуди' in phone_book:
+    if 'Джуди' in phone_book:
         print('А теперь есть. Джуди -', phone_book['Джуди'])
     print(phone_book)
 
@@ -27,5 +27,26 @@ def main():
     print('После удаления записи Джуди: ', phone_book)
     len_pb = len(phone_book)
     print('Количество пар ключ:значение в phone_book: ', len_pb)
+
+    for key in phone_book:
+        print(key, phone_book[key])
+
+    # Некоторые словарные методы
+    # Печатаем все ключи
+    print(phone_book.keys())
+    # Печатаем все значения
+    print(phone_book.values())
+    # Печатаем всё- и ключи и значения, т.е. получаем словарное представление
+    # (особого типа последовательность)
+    print(phone_book.items())
+    # Печатаем значение Мэта, если такого нет - то значение по умолчанию(запись не найдена)
+    print(phone_book.get('Мэт', 'запись не найдена'))
+    print(phone_book.get('Крис', 'Записи нет'))
+
+
+    # Перебор кортежей впоследовательности применив цикл
+    for key, value in phone_book.items():
+        print(key, value)
+
 
 main()
